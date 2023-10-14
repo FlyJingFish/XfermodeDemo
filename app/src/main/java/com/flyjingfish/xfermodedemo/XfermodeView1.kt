3af3a7d8c895c6774aa515280a2c20615b6672ea
FlyJingFish <tianxiangyu168@gmail.com>
2023-10-15 01:03:27 +0800

    更新示例代码

--- app/src/main/java/com/flyjingfish/xfermodedemo/XfermodeView1.kt
+++ app/src/main/java/com/flyjingfish/xfermodedemo/XfermodeView1.kt
@@ -9,24 +9,25 @@ import android.graphics.PorterDuffXfermode
 import android.util.AttributeSet
 import android.view.View
 
-val OVAL_COLOR = Color.YELLOW
-val RECT_COLOR = Color.BLUE
+//val OVAL_COLOR = Color.argb(255,214,57,100)
+//val RECT_COLOR = Color.argb(255,70,150,236)
+val OVAL_COLOR = Color.argb(255,247,205,95)
+val RECT_COLOR = Color.argb(255,117,169,248)
 
 class XfermodeView1(context: Context, attrs: AttributeSet?) : View(context, attrs) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-//    var xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
     var xfermode :PorterDuffXfermode ?= null
         set(value) {
             invalidate()
             field = value
         }
     override fun onDraw(canvas: Canvas) {
-        val count = canvas.saveLayer(100f.dp(),0f.dp(),250f.dp(),150f.dp(),null)
+        val count = canvas.saveLayer(0f,0f,150f.dp(),150f.dp(),null)
         paint.color = OVAL_COLOR
-        canvas.drawOval(150f.dp(),0f,250f.dp(),100f.dp(),paint)
+        canvas.drawOval(0f.dp(),0f,100f.dp(),100f.dp(),paint)
         paint.xfermode = xfermode
         paint.color = RECT_COLOR
-        canvas.drawRect(100f.dp(),50f.dp(),200f.dp(),150f.dp(),paint)
+        canvas.drawRect(50f.dp(),50f.dp(),150f.dp(),150f.dp(),paint)
         paint.xfermode = null
         canvas.restoreToCount(count)
     }
--- app/src/main/java/com/flyjingfish/xfermodedemo/XfermodeView2.kt
+++ app/src/main/java/com/flyjingfish/xfermodedemo/XfermodeView2.kt
@@ -3,9 +3,7 @@ package com.flyjingfish.xfermodedemo
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
 import android.util.AttributeSet
 import android.view.View
@@ -20,27 +18,27 @@ class XfermodeView2(context: Context, attrs: AttributeSet?) : View(context, attr
         }
 
 
-    val rectBitmap =
+    val sourceImage =
         Bitmap.createBitmap(150f.dp().toInt(), 150f.dp().toInt(), Bitmap.Config.ARGB_8888)
-    val ovalBitmap =
+    val destinationImage =
         Bitmap.createBitmap(150f.dp().toInt(), 150f.dp().toInt(), Bitmap.Config.ARGB_8888)
 
     init {
-
-        val canvas = Canvas(ovalBitmap)
+        val canvas = Canvas(destinationImage)
         paint.color = OVAL_COLOR
-        canvas.drawOval(50f.dp(), 0f, 150f.dp(), 100f.dp(), paint)
-        canvas.setBitmap(rectBitmap)
+        //下边起点x是0，是因为我要按照文档的图把圆画到右上角
+        canvas.drawOval(0f, 0f, 100f.dp(), 100f.dp(), paint)
+        canvas.setBitmap(sourceImage)
         paint.color = RECT_COLOR
-        canvas.drawRect(0f.dp(), 50f.dp(), 100f.dp(), 150f.dp(), paint)
-
+        //下边起点y是50，是因为我要按照文档的图把正方形画到左下角
+        canvas.drawRect(50f.dp(), 50f.dp(), 150f.dp(), 150f.dp(), paint)
     }
 
     override fun onDraw(canvas: Canvas) {
-        val count = canvas.saveLayer(100f.dp(), 0f.dp(), 250f.dp(), 150f.dp(), null)
-        canvas.drawBitmap(ovalBitmap, 100f.dp(), 0f.dp(), paint)
+        val count = canvas.saveLayer(0f, 0f, 150f.dp(), 150f.dp(), null)
+        canvas.drawBitmap(destinationImage, 0f, 0f, paint)
         paint.xfermode = xfermode
-        canvas.drawBitmap(rectBitmap, 100f.dp(), 0f.dp(), paint)
+        canvas.drawBitmap(sourceImage, 0f, 0f, paint)
         paint.xfermode = null
         canvas.restoreToCount(count)
     }
